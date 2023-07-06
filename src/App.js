05610ac254b7f3ac54a2dbd422ffe9e756cea2cb
LwynnF <lfong96@hotmail.co.uk>
2023-07-06 19:27:57 +0100

    Added link to repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-	return <div className="App">Weather App</div>;
+	return (
+		<div className="App">
+			<h1>Weather App</h1>
+			<footer>
+				This was coded by wynn.codes{" "}
+				<a
+					href="https://github.com/LwynnF/react-weather-app"
+					target="_blank"
+				>
+					open-sourced on Github
+				</a>
+			</footer>
+		</div>
+	);
 }
