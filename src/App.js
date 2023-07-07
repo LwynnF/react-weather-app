a1901f568d9e4affd23a82504ad2926ed6ae66b9
LwynnF <lfong96@hotmail.co.uk>
2023-07-07 11:21:01 +0100

    Imported bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
 	return (
 		<div className="App">
-			<h1>Weather App</h1>
-			<footer>
-				This was coded by wynn.codes{" "}
-				<a
-					href="https://github.com/LwynnF/react-weather-app"
-					target="_blank"
-					rel="noreferrer"
-				>
-					open-sourced on Github
-				</a>
-			</footer>
+			<div className="container">
+				<h1>Weather App</h1>
+				<footer>
+					This was coded by wynn.codes{" "}
+					<a
+						href="https://github.com/LwynnF/react-weather-app"
+						target="_blank"
+						rel="noreferrer"
+					>
+						open-sourced on Github
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
